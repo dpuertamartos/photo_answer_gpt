c71beffa2e82120e758c77a48a2fc7e17ee700ee
David Puerta Martos <dpuerta@usal.es>
2023-10-22 19:22:01 +0200

    prompts can be send and output received

---
+++ chat_gpt_handler.py
@@ -0,0 +1,49 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class chatGPTHandler:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def send_input(self, prompt):
+        inputElements = self.driver.find_elements(By.TAG_NAME, "textarea")
+
+        inputElements[0].send_keys(prompt)
+        sleep(1)
+        inputElements[0].send_keys(Keys.ENTER)
+
+    def obtain_output(self):
+        # Wait for the button with text "Regenerate" to appear
+        WebDriverWait(self.driver, 90).until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Regenerate')]")))
+
+        # Now that the button has appeared, proceed to fetch the desired paragraphs
+        outputElements = self.driver.find_elements(By.TAG_NAME, "p")[-2::]
+
+        results = []
+        for element in outputElements:
+            results.append(element.text)
+
+        #Clean array of "ChatGPT" non desired text
+        if results and results[-1] == "ChatGPT":
+            results.pop()
+
+        return results[-1]
+
+    def send_prompt(self, prompt):
+        '''
+        :param prompt: String
+        :return output: String
+        '''
+        self.send_input(prompt)
+        return self.obtain_output()
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 import undetected_chromedriver as uc
 from fake_useragent import UserAgent
 import os
 from dotenv import load_dotenv
 from login import logIn
+from chat_gpt_handler import chatGPTHandler
 
 # Load variables from .env
 load_dotenv()
@@ -25,21 +22,11 @@ driver.get("https://chat.openai.com/auth/login")
 login_instance = logIn(driver, os.getenv('MAIL'), os.getenv('PASSWORD'))
 login_instance.usal_login()
 
-inputElements = driver.find_elements(By.TAG_NAME, "textarea")
+chat_gpt = chatGPTHandler(driver)
+result = chat_gpt.send_prompt("¿Qué tal estas?")
 
-i = 0
-# while i<10:
-inputElements[0].send_keys("Hola!, estoy probando")
-sleep(2)
-inputElements[0].send_keys(Keys.ENTER)
-sleep(10)
-inputElements = driver.find_elements(By.TAG_NAME, "p")
-sleep(5)
-results = []
-for element in inputElements:
-   results.append(element.text)
-print(results)
-i+=1
-sleep(5)
+print(result)
 
-driver.quit()
+result2 = chat_gpt.send_prompt("¿Me ayudas?")
+
+print(result2)
