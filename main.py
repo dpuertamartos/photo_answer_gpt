4fc933f321e9e4213532256db837b4aae5677d49
David Puerta Martos <dpuerta@usal.es>
2023-10-22 21:57:11 +0200

    now detects if login was correct to continue or not

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ from chat_gpt_handler import chatGPTHandler
 # Load variables from .env
 load_dotenv()
 
-# Load selenium driver
 selenium_gpt = seleniumGPT()
 driver = selenium_gpt.start_driver("https://chat.openai.com/auth/login")
 
-#Login to chatgpt
 login_instance = logIn(driver, os.getenv('MAIL'), os.getenv('PASSWORD'))
-login_instance.usal_login()
+correct_login = login_instance.login()
 
-#Send prompt1
-chat_gpt = chatGPTHandler(driver)
-result = chat_gpt.send_prompt("¿Qué tal estas?")
+if correct_login:
+    chat_gpt = chatGPTHandler(driver)
+    result = chat_gpt.send_prompt("¿Qué tal estas?")
 
-print(result)
+    print(result)
 
-#Send prompt2
-result2 = chat_gpt.send_prompt("¿Me ayudas?")
-
-print(result2)
+# result2 = chat_gpt.send_prompt("¿Me ayudas?")
+#
+# print(result2)
